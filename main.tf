2110fd097e1184bf291c1efa7931584433dcfac8
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-14 20:28:59 +0700

    Change resource name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_route53_zone" "aws_subdomain" {
   name = var.domain_name
 }
 
-resource "cloudflare_record" "foobar" {
+resource "cloudflare_record" "aws_subdomain_dns" {
   count   = length(aws_route53_zone.aws_subdomain.name_servers)
   zone_id = var.cloudflare_zone_id
   name    = var.domain_name
