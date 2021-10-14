3be241456ef82628e47693e8e2e44e2cf39c7d01
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-14 21:14:52 +0700

    Add datasource

--- main.tf
+++ main.tf
@@ -24,11 +24,15 @@ resource "aws_route53_zone" "aws_subdomain" {
   name = "${var.sub_domain}-${random_integer.random.result}.${var.main_domain}"
 }
 
+data "aws_route53_zone" "aws_subdomain" {
+  name = "${var.sub_domain}-${random_integer.random.result}.${var.main_domain}"
+  depends_on = [aws_route53_zone.aws_subdomain]
+}
+
 resource "cloudflare_record" "aws_subdomain_dns" {
-  count      = length(aws_route53_zone.aws_subdomain.name_servers)
+  count      = length(data.aws_route53_zone.aws_subdomain.name_servers)
   zone_id    = data.cloudflare_zone.main_domain.id
   name       = var.sub_domain
-  value      = aws_route53_zone.aws_subdomain.name_servers[count.index]
+  value      = data.aws_route53_zone.aws_subdomain.name_servers[count.index]
   type       = "NS"
-  depends_on = [aws_route53_zone.aws_subdomain]
 }
