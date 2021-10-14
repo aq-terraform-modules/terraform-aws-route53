2aad14699718041cd5e2de1f0e513bb40d91f3b0
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-14 20:28:42 +0700

    Add CloudFlare creating record

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+
 resource "aws_route53_zone" "aws_subdomain" {
   name = var.domain_name
+}
+
+resource "cloudflare_record" "foobar" {
+  count   = length(aws_route53_zone.aws_subdomain.name_servers)
+  zone_id = var.cloudflare_zone_id
+  name    = var.domain_name
+  value   = aws_route53_zone.aws_subdomain.name_servers[count.index]
+  type    = "NS"
 }
