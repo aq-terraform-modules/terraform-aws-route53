60d566cbc4fe4ded7f963623446bdd8f242df5b4
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 17:42:19 +0700

    Update output for nameserver

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "route53_name_servers" {
+output "name_servers" {
   description = "Output the route53 zone nameservers for troubleshooting"
   value = aws_route53_zone.aws_subdomain.name_servers
 }
