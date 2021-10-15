78f716fde6229ec68ebfc60022b943641c58f9d3
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 14:55:57 +0700

    Update remove random interger

--- main.tf
+++ main.tf
@@ -15,19 +15,14 @@ data "cloudflare_zone" "main_domain" {
   name = var.main_domain
 }
 
-resource "random_integer" "random" {
-  min = 1
-  max = 99999999
-}
-
 resource "aws_route53_zone" "aws_subdomain" {
-  name = "${var.sub_domain}-${random_integer.random.result}.${var.main_domain}"
+  name = "${var.sub_domain}.${var.main_domain}"
 }
 
 resource "cloudflare_record" "aws_subdomain_dns" {
   count      = 4
   zone_id    = data.cloudflare_zone.main_domain.id
-  name       = "${var.sub_domain}-${random_integer.random.result}"
+  name       = "${var.sub_domain}"
   value      = aws_route53_zone.aws_subdomain.name_servers[count.index]
   type       = "NS"
   depends_on = [aws_route53_zone.aws_subdomain]
