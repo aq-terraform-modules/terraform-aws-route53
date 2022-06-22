ed7a43b37d8149518f1fd60656e770bdbcb4c571
Anh Quach <61163704+sheid1309@users.noreply.github.com>
2022-06-23 00:21:08 +0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "name_servers" {
   description = "Output the route53 zone nameservers for troubleshooting"
   value = aws_route53_zone.aws_subdomain.name_servers
 }
+
+output "zone_id" {
+  description = "Zone ID of the Route53 Zone"
+  value = aws_route53_zone.aws_subdomain.zone_id
+}
